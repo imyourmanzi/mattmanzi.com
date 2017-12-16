13636362a060312e6604b473b5c36317f24741fa
Matt Manzi <manzi.matthewr@gmail.com>
2017-12-15 23:59:15 -0500

    started content for Music page

--- src/components/Music.vue
+++ src/components/Music.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="musicContainer" class="container">
-
+        <img src="../../static/img/music-header@0-33x.png"/>
+        <img src="../../static/img/myx-logo.svg"/>
     </div>
 </template>
 
@@ -12,5 +13,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+img {
+    width: 100%;
+}
 </style>
