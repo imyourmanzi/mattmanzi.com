15c80db5ce0435a805396dffded91ca113cf10a0
Matt Manzi <manzi.matthewr@gmail.com>
2017-12-17 23:36:51 -0500

    basic implementation of Resume page from 09-2017 Technical v1_1 resume with collapsing sections

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,16 +1,173 @@
 <template>
     <div id="resumeContainer" class="container">
-
+        <div id="nameplate">
+            <h1>Matthew R. Manzi</h1>
+            ERK 406, UMBC 1000 Hilltop Circle, Baltimore, MD 21250 • (610) 368-4021 • manzi.matthewr@gmail.com
+        </div>
+        <h3 @click="toggleShowSection(resume.ids.education)">Education</h3>
+        <hr />
+        <transition name="reveal">
+            <div v-if="openSections.indexOf(resume.ids.education) !== -1">
+                <div class="education">
+                    <div class="detailLine">
+                        <p><strong>University of Maryland, Baltimore County (UMBC)</strong> Baltimore, MD</p>
+                        <p><em>May 2020</em></p>
+                    </div>
+                    <div class="detailLine">
+                        <p>Bachelor of Science, Computer Science</p>
+                        <p><em>GPA: 4.0</em></p>
+                    </div>
+                    <p><em>Honors:</em> Dean's Fellow, Center for Women in Technology (CWIT) Cyber Scholar</p>
+                </div>
+            </div>
+        </transition>
+        <h3 @click="toggleShowSection(resume.ids.skills)">Technical Skills</h3>
+        <hr />
+        <transition name="reveal">
+            <div v-if="openSections.indexOf(resume.ids.skills) !== -1">
+                <div class="skill" v-for="skill in resume.skills">
+                    <p class="sameWidth"><em>{{ skill.subset }}</em></p>
+                    <p>{{ skill.values.join(", ") }}</p>
+                </div>
+            </div>
+        </transition>
+        <h3 @click="toggleShowSection(resume.ids.experience)">Experience</h3>
+        <hr />
+        <transition name="reveal">
+            <div v-if="openSections.indexOf(resume.ids.experience) !== -1">
+                <div class="experience" v-for="exp in resume.experience">
+                    <div class="detailLine">
+                        <p>{{ exp.employerName }}, <em>{{ exp.title }}</em>, {{ exp.location }}</p>
+                        <p><em>{{ exp.timeDetail }}</em></p>
+                    </div>
+                    <ul>
+                        <li v-for="point in exp.points">{{ point }}</li>
+                    </ul>
+                </div>
+            </div>
+        </transition>
+        <h3 @click="toggleShowSection(resume.ids.projects)">Projects</h3>
+        <hr />
+        <transition name="reveal">
+            <div v-if="openSections.indexOf(resume.ids.projects) !== -1">
+                <div class="project" v-for="proj in resume.projects">
+                    <div class="detailLine">
+                        <p><em>{{ proj.name }}</em>, {{ proj.organization }}</p>
+                        <p><em>{{ proj.timeDetail }}</em></p>
+                    </div>
+                    <ul>
+                        <li v-for="point in proj.points">{{ point }}</li>
+                    </ul>
+                </div>
+            </div>
+        </transition>
+        <h3 @click="toggleShowSection(resume.ids.volunteer)">Volunteer Activities</h3>
+        <hr />
+        <transition name="reveal">
+            <div v-if="openSections.indexOf(resume.ids.volunteer) !== -1">
+                <div class="activity" v-for="activity in resume.volunteer">
+                    <div class="detailLine">
+                        <p><em>{{ activity.name }}</em>, {{ activity.organization }}</p>
+                        <p><em>{{ activity.timeDetail }}</em></p>
+                    </div>
+                    <ul>
+                        <li v-for="point in activity.points">{{ point }}</li>
+                    </ul>
+                </div>
+            </div>
+        </transition>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'resume'
+    name: 'resume',
+    data() {
+        return {
+            openSections: [1]
+        }
+    },
+    computed: {
+        resume() {
+            return this.$store.state.resume
+        }
+    },
+    methods: {
+        toggleShowSection(sectionId) {
+
+            var sectionIndex = this.openSections.indexOf(sectionId)
+
+            if (sectionIndex === -1) {
+                this.openSections.push(sectionId)
+            } else {
+                this.openSections.splice(sectionIndex, 1)
+            }
+
+        }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1 {
+    margin-bottom: 0;
 
+    font-weight: 400;
+}
+
+#nameplate {
+    text-align: center;
+}
+
+h3 {
+    margin-bottom: 0em;
+
+    font-weight: 400;
+    text-transform: uppercase;
+
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    cursor: pointer;
+}
+
+p {
+    margin: 0;
+
+    padding-left: 2em;
+}
+
+ul {
+    margin-top: 0.5em;
+    padding-left: 5em;
+}
+
+.detailLine {
+    display: flex;
+    justify-content: space-between;
+}
+
+.skill {
+    display: flex;
+    justify-content: flex-start;
+}
+
+.skill .sameWidth {
+    width: 11em;
+}
+
+.reveal-enter, .reveal-leave-to {
+    opacity: 0;
+}
+
+.reveal-enter-active {
+    transition: opacity 0.7s;
+}
+
+.reveal-leave-active {
+    transition: opacity 0.2s;
+}
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -53,6 +53,122 @@ export const store = new Vuex.Store({
                 contact: "gvgoldsupport@" + DOMAIN_NAME
             }
         ],
+        resume: {
+            ids: {
+                education: 1,
+                skills: 2,
+                experience: 3,
+                projects: 4,
+                volunteer: 5
+            },
+            skills: [
+                {
+                    subset: "Programming",
+                    values: ["Java", "Swift (iOS)", "Python", "C++", "Transact-SQL", "Bash", "HTML", "CSS", "JavaScript", "C#", "C"]
+                },
+                {
+                    subset: "Operating Systems",
+                    values: ["macOS 9-12", "Ubuntu Server/Desktop 16", "Windows 7/8/10"]
+                },
+                {
+                    subset: "Software",
+                    values: ["MS-Office", "Xcode", "NetBeans", "SQL Server Mgmt Studio", "Splunk (Power User 6.3)", "Eclipse", "VMware"]
+                },
+                {
+                    subset: "Hardware",
+                    values: ["Arduino", "Raspberry Pi", "Rack Servers", "Networking Equipment", "PC Components"]
+                }
+            ],
+            experience: [
+                {
+                    employerName: "UMBC",
+                    title: "Computer Science Teaching Assistant",
+                    location: "Baltimore, MD",
+                    timeDetail: "Aug 2017 – Present",
+                    points: [
+                        "Assist, grade, and support students taking Computer Science I for Majors (CMSC 201)",
+                        "Lead discussion and review sections for students, covering material in Python programming language",
+                        "Hold weekly office hours to aid students outside of class"
+                    ]
+                },
+                {
+                    employerName: "Self-Employed",
+                    title: "iOS Application Developer",
+                    location: "Aston, PA",
+                    timeDetail: "Sep 2014 – Present",
+                    points: [
+                        "Design, develop and publish mobile applications through self-direction",
+                        "Three (3) applications available on the Apple App Store"
+                    ]
+                },
+                {
+                    employerName: "WSFS Bank",
+                    title: "Software Developer Intern",
+                    location: "Wilmington, DE",
+                    timeDetail: "Jun – Aug 2017",
+                    points: [
+                        "Worked with 3 developers and 2 project managers over a 3-sprint Agile project to develop and deliver a working web-based check processing application to an internal customer",
+                        "Co-designed and implemented the RESTful API and database for the application",
+                        "Participated in daily standup meetings with team members to discuss progress, speed bumps, and successes of the current sprint and overall project"
+                    ]
+                },
+                {
+                    employerName: "Cratr LLC",
+                    title: "Senior Programmer",
+                    location: "Glen Mills, PA",
+                    timeDetail: "Aug 2016 – May 2017",
+                    points: [
+                        "Built and integrated features to provide crucial website functionality and benefit user experience, including a payment system and product pages",
+                        "As a member of the management team and co-owner, collaborated with 5 team members to facilitate personal and other’s contributions to the formation of the business and startup of the company"
+                    ]
+                }
+            ],
+            projects: [
+                {
+                    name: "Innovate Good Hackathon",
+                    organization: "UMBC",
+                    timeDetail: "Apr 2017",
+                    points: [
+                        "Collaborated with 3 students to brainstorm, develop, launch, and present a winning web application (using Django API in Python on a Heroku app) in a 24-hour time constraint, aimed to help crime victims"
+                    ]
+                },
+                {
+                    name: "HackUMBC Hackathon",
+                    organization: "UMBC",
+                    timeDetail: "Oct 2016",
+                    points: [
+                        "Collaborated with 3 students to brainstorm, develop, launch, and present an original website in a 24-hour time constraint to provide users with crowdsourced restaurant wait times"
+                    ]
+                },
+                {
+                    name: "NSA 4th Codebreaker Challenge",
+                    organization: "UMBC",
+                    timeDetail: "Sep – Dec 2016",
+                    points: [
+                        "Independently solved challenges mimicking real-world tasks to learn various computer security techniques through network-based and reverse engineering tasks"
+                    ]
+                }
+            ],
+            volunteer: [
+                {
+                    name: "Bits and Bytes",
+                    organization: "UMBC CWIT",
+                    timeDetail: "Feb 2017",
+                    points: [
+                        "Assisted participants in a programming workshop intended to promote STEM fields to high school junior girls"
+                    ]
+                },
+                {
+                    name: "Community Outreach",
+                    organization: "GVHS Robotics Team",
+                    timeDetail: "Jul – Oct 2015",
+                    points: [
+                        "Developed and taught two 2-week summer programming camps",
+                        "Designed, developed and published mobile (iOS) guided tour application for the school’s 50th Homecoming"
+                    ]
+                }
+            ]
+        },
         faqEntries: [
             {
                 id: 1,
@@ -85,7 +201,7 @@ export const store = new Vuex.Store({
             }
 
         }
-        
+
     },
     mutations: {
         // add mutations for state data here
