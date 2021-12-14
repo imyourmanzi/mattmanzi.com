67b3fdf08c8590d80f01e1b97a82929c8a824f2d
Matt Manzi <imyourmanzi1@gmail.com>
2021-12-14 15:45:57 -0500

    Finish porting App layout to Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,215 @@
 <script lang="ts">
-  export let name: string;
+  // temp
+  const pageName = 'home';
+  // end temp
+
+  const sections = [
+    {
+      title: 'Tech',
+      destination: '/tech',
+    },
+    {
+      title: 'Photos',
+      destination: '/photos',
+    },
+    {
+      title: 'Résumé',
+      destination: '/resume',
+    },
+  ];
+
+  const socialLinks = [
+    {
+      imgResource: 'ext_git.svg',
+      imgAlt: 'Link to GitHub profile',
+      url: 'https://github.com/imyourmanzi',
+    },
+    {
+      imgResource: 'ext_ig.png',
+      imgAlt: 'Link to Instagram profile',
+      url: 'https://www.instagram.com/imyourmanzi/',
+    },
+    {
+      imgResource: 'ext_fb.svg',
+      imgAlt: 'Link to Facebook profile',
+      url: 'https://www.facebook.com/imyourmanzi',
+    },
+    {
+      imgResource: 'ext_tw.svg',
+      imgAlt: 'Link to Twitter profile',
+      url: 'https://twitter.com/imyourmanzi',
+    },
+    {
+      imgResource: 'ext_in.png',
+      imgAlt: 'Link to LinkedIn profile',
+      url: 'https://www.linkedin.com/in/mattrmanzi/',
+    },
+  ];
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build
-    Svelte apps.
-  </p>
-</main>
+<header class:dividedTop="{pageName !== 'home'}">
+  <a id="siteTitleAnchor" href="/">
+    <span id="siteTitle">MattManzi<span id="dotCom">.com</span></span>
+  </a>
+  <div id="layoutLinks">
+    {#each sections as section (section.destination)}
+      <a
+        class="layoutLink"
+        class:currentPage="{pageName === section.destination}"
+        href="{section.destination}"
+      >
+        {section.title}
+      </a>
+    {/each}
+  </div>
+</header>
+<!-- <main><router-view></router-view></main> -->
+<footer class:dividedBottom="{pageName !== 'home'}">
+  <div id="socialLinks">
+    {#each socialLinks as social (social.url)}
+      <a href="{social.url}" class="socialLink" rel="noopener" target="_blank">
+        <img src="{`img/${social.imgResource}`}" alt="{social.imgAlt}" />
+      </a>
+    {/each}
+  </div>
+  <br />
+  <span
+    >👨🏻‍💻 with ❤️ by
+    <a
+      class="layoutLink"
+      href="https://github.com/imyourmanzi/MattManzi-UI"
+      target="_blank"
+      rel="noopener">imyourmanzi</a
+    >
+  </span>
+</footer>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  /* Layout attributes */
+  header {
+    position: relative;
+    width: 92%;
+    padding: 2% 4% 1% 4%;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  #siteTitleAnchor:visited,
+  #siteTitleAnchor:hover,
+  #siteTitleAnchor:active {
+    color: inherit;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
+  #siteTitle {
+    font-size: 3em;
+    font-weight: 300;
+  }
+
+  #siteTitle > #dotCom {
+    font-size: 50%;
+  }
+
+  .dividedTop {
+    border-bottom: 1px solid gray;
+
+    border-radius: 0;
+  }
+
+  .dividedBottom {
+    border-top: 1px solid gray;
+
+    border-radius: 0;
+  }
+
+  #layoutLinks {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .layoutLink {
+    padding: 0em 1em;
+
+    font-size: 1.25em;
+    color: inherit;
+  }
+
+  .layoutLink:hover {
+    color: gray;
+    text-decoration: underline;
+  }
+
+  .currentPage {
+    text-decoration: underline !important;
+  }
+
+  #socialLinks {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+
+    padding: 1em 1em;
+  }
+
+  .socialLink {
+    padding: 0 1.5em;
+
+    cursor: pointer;
+  }
+
+  .socialLink:hover {
+    opacity: 0.5;
+  }
+
+  .socialLink img {
+    height: 32px;
+  }
+
+  footer {
+    width: 100%;
+    bottom: 0;
+    padding: 0 0 1em 0;
+
+    font-size: 0.82em;
+  }
+
+  footer .layoutLink {
+    padding: 0;
+
+    font-size: 100%;
+  }
+
+  /* Extra tall and skinny screens (i.e. smartphones) */
+  @media screen and (max-aspect-ratio: 767/1024) {
+    #layoutLinks {
+      justify-content: space-around;
+      overflow-x: auto;
+      -webkit-overflow-scrolling: touch;
+      -ms-overflow-style: -ms-autohiding-scrollbar;
+    }
+
+    .layoutLink {
+      flex: 0 0 auto;
+      padding: 0em 0.6em;
+    }
+
+    #socialLinks {
+      justify-content: space-around;
+
+      padding: 1em 0;
+    }
+
+    .socialLink {
+      padding: 0;
+    }
+  }
+
+  /* Light mode vs. dark mode (default) */
+  @media screen and (prefers-color-scheme: light) {
+    .socialLink img {
+      filter: invert();
     }
   }
 </style>
