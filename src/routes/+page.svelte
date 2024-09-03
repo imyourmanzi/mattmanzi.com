337a5387e2556d4d5cbff73af0831a22612cdf0d
Matt Manzi <imyourmanzi1@gmail.com>
2024-09-03 16:54:14 -0400

    MFE-107 use markdown for profile text
    also include edits from Dad

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,27 @@
 <script lang="ts">
+  import { Marked } from 'marked';
+  import profileText from './profile_text.md?raw';
+  import DOMPurify from 'dompurify';
+
+  const projectMarkdownParser = new Marked();
+  projectMarkdownParser.use({
+    hooks: {
+      postprocess(html) {
+        return html.replaceAll('<a', '<a target="_blank" rel="external noreferrer" ');
+      }
+    }
+  });
+
+  const loadProfileText = async () => {
+    const parsed = await projectMarkdownParser.parse(profileText);
+
+    const sanitized = DOMPurify.sanitize(parsed, {
+      USE_PROFILES: { html: true, svg: true, svgFilters: true },
+      ADD_ATTR: ['target']
+    });
+
+    return sanitized;
+  };
 </script>
 
 <div id="homeContainer" class="container">
@@ -26,47 +49,16 @@
         >
       </h1>
       <div>
-        <p>Versatile, Self-Driven Technology Solutions Developer</p>
+        <p>Versatile & Self-Driven Technology Solutions Developer</p>
         <p id="profileQuote">"As we say it, so it shall be."</p>
       </div>
     </div>
   </div>
-  <div id="profileText">
-    <p>
-      Hi, I’m Matt Manzi, a self-driven, hardworking technology solutions developer. And a
-      smart home enthusiast, Mandarin learner, rock climber, traveler, and dog lover. In
-      2020, I graduated Summa Cum Laude from the University of Maryland, Baltimore County
-      with a Bachelor of Science in Computer Science, focusing on Cybersecurity, and a
-      Certificate of Chinese Studies.
-    </p>
-    <p>
-      By profession, I’m a software engineer. By interest, I’m a lot more. Since
-      childhood, I’ve been driven to learn about all the different kinds of technology
-      that power our lives: electronic & mechanical, software & hardware, cameras & audio,
-      custom systems & open source. Along the way I picked up hobbies in filmmaking &
-      photography, DJing, learning Mandarin, rock climbing, driving manuals, and
-      traveling.
-    </p>
-    <p>
-      I work as a full-stack software engineer building SaaS applications on AWS. I
-      contribute to every aspect of the cloud and local tech stack: <strong
-        >TypeScript & JavaScript, AWS Lambda & AWS ECS</strong
-      >
-      to write server applications in <strong>Node.js</strong>, frontend applications in
-      <strong>React & Angular</strong>,
-      <strong>Docker, Terraform, & AWS CloudFormation</strong>
-      to deploy those applications, <strong>Python & Datadog</strong> to optimize
-      observability, logging, and auditing workflows, and
-      <strong>Bash scripting & various developer tooling</strong> to improve the development
-      experience. To challenge myself, I recently began contributing to open source projects
-      I use, and I’m learning about operating systems and OS-level software development.
-    </p>
-    <p id="profileContactLine">
-      If you think I might be a good fit for your team, let’s chat! Give me a call or send
-      me a note: <a href="tel:14433847455">(443) 384-7455</a> or
-      <a href="mailto:manzi.mattr@gmail.com">manzi.mattr@gmail.com</a>.
-    </p>
-  </div>
+  {#await loadProfileText() then loadedProfileText}
+    <div id="profileText">
+      {@html loadedProfileText}
+    </div>
+  {/await}
 </div>
 
 <style>
